80f58609d85261251cf336d9e05bb8b644d9085d
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-12 21:16:54 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,12 +3,12 @@ Test goes here
 
 """
 
-import pandas as pd
+import polars as pl
 from main import development
 
 def test_data():
     # Test with dataset
-    data= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+    data= pl.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
 )
     result = development(data)
     assert result == 47.790116494845364
